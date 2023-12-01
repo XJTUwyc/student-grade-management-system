6a737887da883473461b1025f9d80c65b0e58fad
XJTUwyc <73230439+XJTUwyc@users.noreply.github.com>
2023-12-01 23:35:28 +0800

    Add files via upload
    springboot

---
+++ springboot/src/main/java/com/example/service/AdminService.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import com.example.entity.Admin;
+import com.example.exception.CustomException;
+import com.example.mapper.AdminMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class AdminService {
+
+    @Resource
+    private AdminMapper adminMapper;
+    /**
+     * 登录
+     */
+    public Admin login(Admin admin) {
+        Admin dbAdmin = adminMapper.selectByUsername(admin.getUsername());
+        if (dbAdmin == null) {
+            // 未查询到用户信息
+            throw new CustomException("账号或密码错误");
+        }
+        // 比较密码
+        if (!admin.getPassword().equals(dbAdmin.getPassword())) {
+            throw new CustomException("账号或密码错误");
+        }
+        // 登录成功
+        return dbAdmin;
+    }
+}
